27ead6c83f6518bfc3a40422a4549417c151af49
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-04-15 18:39:07 +0500

    CODE RUB: Add Customers , Add Icons , Add pictures

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -7,5 +7,21 @@ import { customers } from '../customersData';
   styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent {
-  customers= customers
+  customers = customers;
+  currentPage = 1;
+  pageSize = 12;
+  totalPages = Math.ceil(this.customers.length / this.pageSize);
+  pages: number[] = [];
+
+  get customersForCurrentPage() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.customers.slice(startIndex, endIndex);
+  }
+
+  constructor() {
+    for (let i = 1; i <= this.totalPages; i++) {
+      this.pages.push(i);
+    }
+  }
 }
--- src/app/customersData.ts
+++ src/app/customersData.ts
@@ -1,70 +1,156 @@
 export interface CustomersData {
-    id: number;
-    name: string;
-    address: string;
-    details: string;
-  }
-  
-  export const customers = [
-    {
-      id:1,
-      name: 'Ted James',
-      address: 'Phonix, Arizona',
-      details: 'Wiew Orders'
-    },
-    {
-      id:2,
-      name: 'Michelle Thompson',
-      address: 'Encinitas, California',
-      details: 'Wiew Orders'
-    },
-    {
-      id:3,
-      name: 'Zed Bishop',
-      address: 'Seattle, Washington',
-      details: 'Wiew Orders'
-    },
-    {
-      id:4,
-      name: 'Tina Adams',
-      address: 'Chandler, Arizona',
-      details: 'Wiew Orders'
-    },
-    {
-      id:5,
-      name: 'Igor Minar',
-      address: 'Dallas, Texas',
-      details: 'Wiew Orders'
-    },
-    {
-      id:6,
-      name: 'Brad Green',
-      address: 'Orlando, Florida',
-      details: 'Wiew Orders'
-    },
-    {
-      id:7,
-      name: 'Misko Hevery',
-      address: 'Carey, North Calorina',
-      details: 'Wiew Orders'
-    },
-    {
-      id:8,
-      name: 'Heedy Wahlin',
-      address: 'Anaheim, California',
-      details: 'Wiew Orders'
-    },
-    {
-      id:9,
-      name: 'John Papa',
-      address: 'Orlando, Florida',
-      details: 'Wiew Orders'
-    },
-    {
-      id:10,
-      name: 'Tonya Smith',
-      address: 'Atlanta, Georgia',
-      details: 'Wiew Orders'
-    },
-    
-  ]
+  id: number;
+  name: string;
+  address: string;
+  details: string;
+  avatar: string;
+}
+
+export const customers = [
+  {
+    id:1,
+    name: 'Ted James',
+    address: 'Phonix, Arizona',
+    details: 'View Orders',
+    avatar: 'assets/images/boss.png'
+  },
+  {
+    id:2,
+    name: 'Michelle Thompson',
+    address: 'Encinitas, California',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:3,
+    name: 'Zed Bishop',
+    address: 'Seattle, Washington',
+    details: 'View Orders',
+    avatar: 'assets/images/boss.png'
+  },
+  {
+    id:4,
+    name: 'Tina Adams',
+    address: 'Chandler, Arizona',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:5,
+    name: 'Igor Minar',
+    address: 'Dallas, Texas',
+    details: 'View Orders',
+    avatar: 'assets/images/boss.png'
+  },
+  {
+    id:6,
+    name: 'Brad Green',
+    address: 'Orlando, Florida',
+    details: 'View Orders',
+    avatar: 'assets/images/boss.png'
+  },
+  {
+    id:7,
+    name: 'Misko Hevery',
+    address: 'Carey, North Colorina',
+    details: 'View Orders',
+    avatar: 'assets/images/boss.png'
+  },
+  {
+    id:8,
+    name: 'Heady Wahlin',
+    address: 'Anaheim, California',
+    details: 'view Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:9,
+    name: 'John Papa',
+    address: 'Orlando, Florida',
+    details: 'View Orders',
+    avatar: 'assets/images/boss.png'
+  },
+  {
+    id:10,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+
+  {
+    id:11,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+
+  {
+    id:12,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+
+  {
+    id:13,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+
+  {
+    id:14,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+
+  {
+    id:15,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+
+  {
+    id:16,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:17,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:18,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:19,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+  {
+    id:20,
+    name: 'Tonya Smith',
+    address: 'Atlanta, Georgia',
+    details: 'View Orders',
+    avatar: 'assets/images/woman.png'
+  },
+]
